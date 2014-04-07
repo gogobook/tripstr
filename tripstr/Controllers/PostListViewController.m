53d879526d97f83606537fa702ccf9cccdabc509
DavidWang <ctwsine@gmail.com>
2014-04-07 23:21:44 +0800

    constraints conflict issue on postListViewController fixed

--- tripstr/Controllers/PostListViewController.m
+++ tripstr/Controllers/PostListViewController.m
@@ -14,28 +14,42 @@
 #import "AddPostViewController.h"
 
 
-@interface PostListViewController () <UITableViewDataSource,UITableViewDelegate>
+@interface PostListViewController () <UITableViewDataSource,UITableViewDelegate,PostModelDelegate>
 
 @property (nonatomic,strong) PostListTableView* tableView;
+
+@property (nonatomic,strong) PostModel* post;
+@property (nonatomic,strong) NSArray* postList;
+
 
 @end
 
 @implementation PostListViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    [self setLayout];
+    [self setupConstraints];
+    [self.post fetchPostListAll];
+
+}
+
+-(void) setLayout
+{
     [self.view addSubview:self.tableView];
+    self.navigationController.navigationBar.translucent = NO;
+    
+    
+    
+}
+
+- (void) setupConstraints
+{
+    NSDictionary* dict =@{@"tableView":self.tableView};
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[tableView]|" options:0 metrics:nil views:dict]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[tableView]|" options:0 metrics:nil views:dict]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,28 +57,40 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)addPostTapped:(id)sender {
-//    AddPostViewController* apvc = [[AddPostViewController alloc] init];
-//    [self.navigationController pushViewController:apvc animated:YES];
-    //[self presentViewController:apvc animated:YES completion:nil];
+
+#pragma mark- setter
+-(void)setPostList:(NSArray *)postList
+{
+    _postList = postList;
+    [self.tableView reloadData];
 }
 
 #pragma mark - getter
 -(PostListTableView *)tableView
 {
     if (!_tableView) {
-        _tableView = [[PostListTableView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+        _tableView = [[PostListTableView alloc] init];
+        _tableView.translatesAutoresizingMaskIntoConstraints = NO;
         _tableView.delegate = self;
         _tableView.dataSource = self;
     }
     return _tableView;
 }
 
+-(PostModel *)post
+{
+    if (!_post) {
+        _post = [[PostModel alloc] init];
+        _post.delegate = self;
+    }
+    return _post;
+}
+
 #pragma mark - tableView DataSource
 
 -(NSInteger)tableView:(PostListTableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return self.postList.count;
 }
 
 -(PostListCell *)tableView:(PostListTableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -74,8 +100,9 @@
     if (!cell) {
         cell = [[PostListCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
     }
-    cell.textLabel.text = @"Title";
-    cell.detailTextLabel.text = @"detail";
+    PostModel* post = self.postList[indexPath.row];
+    cell.textLabel.text = post.headline;
+    cell.detailTextLabel.text = post.content;
     return cell;
 }
 
@@ -84,4 +111,16 @@
     [self performSegueWithIdentifier:@"PostListToPostSegue" sender:nil];
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+#pragma mark- postModel delegate
+-(void)didFetchDataAll:(NSMutableArray *)postList
+{
+    self.postList = postList;
+}
+
+-(void)failToFetchDataAll:(NSError *)error
+{
+    NSLog(@"plvc error: %@",error);
+}
+
 @end
